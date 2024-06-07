0a406f5c54f5d0a7189566f44bc83f4087410aab
Ashaya01 <ashaysah@gmail.com>
2024-06-07 12:02:41 +0545

    calling the api with async await method

--- js/script.js
+++ js/script.js
@@ -2,20 +2,41 @@ const apiBody = document.querySelector(".api_body");
 
 const apiUrl = "https://icanhazdadjoke.com/";
 
-const fetchData = () => {
-    fetch(apiUrl,{
+//   -> Use of fetch(api) then and catch method to get data and trim down to the actual needed data <- 
+// const fetchData = () => {
+//     fetch(apiUrl,{
+//         headers: {
+//             Accept: "application/json",
+//         },
+//     }).then((response)=>{
+//         return response.json();
+//     }).then((data)=> {
+//         // console.log(data);
+//         apiBody.innerText = data.joke; 
+//     }).catch((error)=> {
+//         apiBody.innertext = "Api is not working";
+//         console.log('Api is not working');
+//     });
+// };
+
+
+// -> Using the async await method to get the data from the api <-
+const fetchData = async () => {
+    try{
+    let responseFromApi = await fetch(apiUrl, {
         headers: {
-            Accept: "application/json",
+            Accept: 'application/json',
         },
-    }).then((response)=>{
-        return response.json();
-    }).then((data)=> {
-        // console.log(data);
-        apiBody.innerText = data.joke; 
-    }).catch((error)=> {
-        apiBody.innertext = "Api is not working";
-        console.log('Api is not working');
     });
+    let jsonData = await responseFromApi.json();
+    // console.log(jsonData.joke);
+
+    apiBody.innerText = jsonData.joke;
+    } catch(error){
+        // console.log(error);
+        apiBody.innerText = error;
+
+    }
 };
 
 fetchData();
